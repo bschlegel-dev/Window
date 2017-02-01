6cab8e8fa754ab12e927a2b60e265034d12d7f5c
Marc Rousavy <Marc Rousavy>
2017-02-01 22:46:53 +0100

    Added initial files

---
+++ Window/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+
+namespace BenWindow {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            this.Width = SystemParameters.WorkArea.Width;
+            this.Height = SystemParameters.WorkArea.Height;
+            this.Top = SystemParameters.WorkArea.Top;
+            this.Left = SystemParameters.WorkArea.Left;
+
+            this.Loaded += async delegate {
+                await Task.Delay(250);
+                toast.Visibility = Visibility.Visible;
+
+                //REMOVE THIS
+                await Task.Delay(1000);
+                toast.Show("Hallo!");
+            };
+        }
+    }
+}
